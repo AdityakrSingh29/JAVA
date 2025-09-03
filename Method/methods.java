ca98dfc96d1edaabb2dce654ec2e75bc63b430d9
ADITYA KUMAR SINGH <143291559+AdityakrSingh29@users.noreply.github.com>
2025-09-03 15:45:24 +0530

    LEARNING ABOUT METHODS IN JAVA

---
+++ Method/methods.java
@@ -0,0 +1,18 @@
+//package com.ok.java.Method;
+//
+//public class methods {
+//    public static void main(String [] args){
+//        methods m=new methods();
+//        int r=Sum(10,20);//CALLING
+////    static int Sum(int a,int b) //METHOD DEFN
+////    {
+////        return a+b;
+////    }
+////
+////    int Sum(int a,int b) //METHOD DEFN
+////    {
+////        return a+b;
+////    }
+//
+//
+//}
