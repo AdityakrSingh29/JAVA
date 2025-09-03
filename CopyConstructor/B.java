836e07d11660ae9f4d4bfde382f343418d2ede01
ADITYA KUMAR SINGH <143291559+AdityakrSingh29@users.noreply.github.com>
2025-09-03 15:34:07 +0530

    LEANING ABOUT COPY CONSTRUCTOR IN JAVA

---
+++ CopyConstructor/B.java
@@ -0,0 +1,26 @@
+//COPY CONSTRUCTOR
+class A 
+{
+    int a;
+    String b;
+    A()
+    {
+        a=10;
+        b="AdityaKrSingh";
+        System.out.println(a+" "+b);
+    }
+    A(A ref)
+    {
+        a=ref.a;
+        b=ref.b;
+        System.out.println(a+" "+b);
+    }
+}
+class B
+{
+    public  static void main(String [] args){
+        A r=new A();
+        A r2=new A(r);
+
+    }
+}
