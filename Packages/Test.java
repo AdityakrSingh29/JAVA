b736cae02afbd3f61c201a964f733e8affbad3e9
ADITYA KUMAR SINGH <143291559+AdityakrSingh29@users.noreply.github.com>
2025-09-10 20:31:48 +0530

    PACKAGES IN JAVA

---
+++ Packages/Test.java
@@ -0,0 +1,16 @@
+//USER DEFINED PACKAGE.
+// javac -d . Test.java
+package aditya;
+class Test
+{
+    private void show()
+    {
+        System.out.println("Learn coding....");
+    }
+    public static void main(String[] args) {
+        Test r=new Test();
+        r.show();
+    }
+}
+
+//PRIVATE-access modifer
---
+++ Packages/Test1.java
@@ -0,0 +1,17 @@
+//USER DEFINED PACKAGE.
+// javac -d . Test.java
+package aditya;
+class Test
+{
+    void show()
+    {
+        System.out.println("Learn coding....");
+    }
+}
+class Test1
+{
+    public static void main(String[] args) {
+        Test1 a=new Test1();
+        a.show();
+    }
+}
